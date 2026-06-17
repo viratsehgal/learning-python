72ad5917b64b5305b46cab5fbf4627b5bd46443e
Virat <viratsehgal.work@gmail.com>
2026-06-18 01:43:22 +0530

    Area and circuference finder

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def vowel_count(phrase) : 
-  count = 0
-  for i in phrase:
-    if i in "AEIOUaeiou":
-      count += 1
-  return count
-
-print(vowel_count(input("Enter a sentence: ")))
+import math
+def calculate_area(radius):
+    area = math.pi * radius ** 2
+    return area 
+def calculate_circumference(radius):
+    circumference = 2 * math.pi * radius
+    return circumference
+radius = float(input("Enter the radius of the circle: "))
+area = calculate_area(radius)
+circumference = calculate_circumference(radius)
+print("The area of the circle with radius " , radius , "is" , area)
+print("The circumference of the circle with radius " , radius , "is" , circumference)
