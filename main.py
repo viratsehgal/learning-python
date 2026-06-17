746aa855af0535098b46417854d1ae60c6e39a7d
Virat <viratsehgal.work@gmail.com>
2026-06-17 12:17:35 +0530

    vowel count problem

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello, Virat!")
+def vowel_count(phrase) : 
+  count = 0
+  for i in phrase:
+    if i in "AEIOUaeiou":
+      count += 1
+  return count
+
+print(vowel_count(input("Enter a sentence: ")))
