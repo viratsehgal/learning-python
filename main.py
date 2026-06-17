e2dc58be4ef2d592de95b577d4f4d2eabf332807
Virat <viratsehgal.work@gmail.com>
2026-06-18 04:13:42 +0530

    List stastistics

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-sentence_input = input("Enter a sentence: ")
-print(sentence_input.title())
-print(sentence_input.upper())
-print(sentence_input.lower())
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter a number: "))
+num3 = float(input("Enter a number: "))
+num4 = float(input("Enter a number: "))
+num5 = float(input("Enter a number: "))
+num6 = float(input("Enter a number: "))
+num7 = float(input("Enter a number: "))
+num8 = float(input("Enter a number: "))
+
+entered_numbers = [num1 ,num2 ,num3 ,num4 ,num5 ,num6 ,num7 ,num8]
+
+highest_number = max(entered_numbers)
+lowest_number = min(entered_numbers)
+average = sum(entered_numbers) / len(entered_numbers)
+ascending_order = sorted(entered_numbers)
+
+print("Highest number in the list is:" , highest_number)
+print("Lowest number in the list is:" , lowest_number)
+print("Average of all the numbers in the list is:" , average)
+print("The list in ascending order:" , ascending_order)
