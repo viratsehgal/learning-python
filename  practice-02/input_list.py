2c2f3ad50b0a1cd1978558f856f4f6f24eb629cf
Virat <viratsehgal.work@gmail.com>
2026-06-20 03:13:58 +0530

    Input in list

---
+++  practice-02/input_list.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+num3 = int(input("Enter a number: "))
+num4 = int(input("Enter a number: "))
+num5 = int(input("Enter a number: "))
+
+num_input = [num1 , num2 , num3 , num4 , num5]
+print(num_input)
+print(num_input[::-1])
