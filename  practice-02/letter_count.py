46e8aa87fcdc04faa636a6cb8391d2a517fcafaa
Virat <viratsehgal.work@gmail.com>
2026-06-20 03:21:28 +0530

    Letter count

---
+++  practice-02/letter_count.py
@@ -0,0 +1,5 @@
+input_word = input("Enter a word: ")
+input_letter = input("Enter a letter: ")
+
+letter_count = input_word.count(input_letter)
+print("The amount of times",input_letter,"appeared in",input_word,"is:",letter_count)
