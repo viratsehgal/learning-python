467b032ae75133c3da9b2de89731ad281db58cc1
Virat <viratsehgal.work@gmail.com>
2026-06-23 10:24:29 +0530

    hotfix: fix row level variable

---
+++ practice-03/multiplication_table.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the number of rows (n): "))
+r = int(input("Enter the range limit (r): "))
+
+width = len(str(n * 2)) + 2
+
+for i in range(1, n + 1):
+    for j in range(1, r + 1):
+        print(f"{i * j :> {width}}" , end="")
+    print()
